fc64a0fa751fa9cf92ea9ef39403c66686d6c451
Hiếu <74406544+HMit19@users.noreply.github.com>
2022-03-16 12:08:35 +0700

    add labrary

---
+++ Library/io.h
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define REP(i, a, b) for(int i = a; i < b; i++)
+#define ll long long
+#define endl "\n"
+using namespace std;
+void io()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.inp", "r", stdin);
+    freopen("output.out", "w", stdout);
+}
